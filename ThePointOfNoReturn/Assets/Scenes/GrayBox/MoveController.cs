89966d46c9071d068cb818c4a2f4cffb74436957
CreatorLynx <1_linx_1@mail.ru>
2025-11-08 23:19:27 +0300

    FIRST BASIC FP CONTROLLER OF PLAYER
    this controller of course need to have some changes in future
    development. it is only basic, first version
    
    also:
    - tweaks of skybox (not realized)
    - graybox scene and lightning on it
    - tweaks in input (dont remember it)
    - test material
    - added fog to lightning on graybox scene (want to use  it in gameplay
      by default. cool style)

--- ThePointOfNoReturn/Assets/Scenes/GrayBox/MoveController.cs
+++ ThePointOfNoReturn/Assets/Scenes/GrayBox/MoveController.cs
@@ -1,35 +1,84 @@
+using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class MoveController : MonoBehaviour
 {
+    //redisign
+    [SerializeField] InputActionAsset InputActions;
+    Vector2 moveVector;
+    Vector2 lookVector;
+
 
     //basics
     Rigidbody _rigidbody;
-    InputAction moveAction, jumpAction;
+    [SerializeField] Transform _cameraTransform;
+    InputAction moveAction;
+    InputAction jumpAction;
+    InputAction lookAction;
 
     //variable
-    [SerializeField]
-    float movementSpeed = 5f;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float lookSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
-    //temporary testing
-    [SerializeField]
-    Text testText;
+    void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+    }
+    void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+    }
 
 
-    void Start()
+    void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        moveAction = InputSystem.actions.FindAction("Player/Move");
+        moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        lookAction = InputSystem.actions.FindAction("Look");
     }
 
     void Update()
     {
-        Vector2 moveVector = moveAction.ReadValue<Vector2>();
-        testText.text = moveVector.ToString() + "  " + Keyboard.current.wKey.wasPressedThisFrame.ToString();
+        moveVector = moveAction.ReadValue<Vector2>();
+        lookVector = lookAction.ReadValue<Vector2>();
 
-        _rigidbody.AddForce(moveVector * movementSpeed * Time.deltaTime);
+        if (jumpAction.WasPressedThisFrame()) Jump();
+
+        Looking();
+    }
+
+    void FixedUpdate()
+    {
+        Moving();
+    }
+
+    void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.VelocityChange);
+    }
+
+    void Moving()
+    {
+        _rigidbody.MovePosition(
+            _rigidbody.position +
+            transform.forward * moveVector.y * movementSpeed * Time.fixedDeltaTime + 
+            transform.right * moveVector.x * movementSpeed * Time.fixedDeltaTime);
+    }
+    
+    void Looking()
+    {
+        float rotationPlayerY = lookVector.x * lookSpeed * Time.deltaTime;
+        Quaternion deltaRotation = Quaternion.Euler(0, rotationPlayerY, 0);
+        //_rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+        transform.Rotate(deltaRotation.eulerAngles);
+
+        float rotationCameraX = lookVector.y * lookSpeed * Time.deltaTime;
+        Quaternion deltaXRotation = Quaternion.Euler(-rotationCameraX, 0, 0);
+        if (_cameraTransform.rotation.x < 90f && _cameraTransform.rotation.x > -90f)
+            _cameraTransform.Rotate(deltaXRotation.eulerAngles);
     }
 }
